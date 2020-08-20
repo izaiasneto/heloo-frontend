bd7f5ea753148c9750157a59d821729f7d411816
Izaias neto <izaias_neto_@live.com>
2020-08-19 21:15:23 -0300

    retirando validity desnecessaria

--- src/containers/CupomForm/index.js
+++ src/containers/CupomForm/index.js
@@ -118,14 +118,6 @@ class CupomForm extends Component {
         if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
-        
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
        
         if (rules.isCupom) {
             const pattern = /(?=.*[a-zA-Z])/;
--- src/containers/EditForm/index.js
+++ src/containers/EditForm/index.js
@@ -119,13 +119,6 @@ class EditForm extends Component {
             isValid = value.trim() !== '' && isValid;
         }
         
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
        
         if (rules.isCupom) {
             const pattern = /(?=.*[a-zA-Z])/;
